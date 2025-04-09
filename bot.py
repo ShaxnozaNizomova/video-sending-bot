5b55f0a2d0b41a55e76ae7408193f147b10f4f5e
ShaxnozaNizomova <shaxnozanizomova91@gmail.com>
2025-04-09 09:56:40 +0500

    delete dispatch

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import logging
 from flask import Flask, request
 from telegram import Update
-from telegram.ext import Application, Dispatcher, CallbackContext
+from telegram.ext import Application, CallbackContext
 from user import user_handlers
 from admin import admin_handlers
 
